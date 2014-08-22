0ba876a182142ccb35451193f55099a6639439e5
harryhychu <chy-2@hotmail.com>
2014-08-23 00:36:40 +0800

    commit routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,13 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-<<<<<<< HEAD
-$route['default_controller'] = "home";
-$route['404_override'] = '';
 
-
-/* End of file routes.php */
-/* Location: ./application/config/routes.php */
-=======
 $route['default_controller'] = "common/home";
 $route['404_override'] = 'common/notFound';
->>>>>>> origin/master
+
