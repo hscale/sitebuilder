c0b4ff5e65f46e9086623b131eecd282044a16d7
auphone <soulxdxd@gmail.com>
2014-08-26 22:58:45 +0800

    Add Context Menu

--- application/views/builder/build.php
+++ application/views/builder/build.php
@@ -21,7 +21,7 @@
 
 <div id="builder-component">
 	<button class="draggable">Submit</button>
-    <button class="draggable">Back</button>
+    <button class="draggable" id="test">Back</button>
     <form class="draggable droppable-form">
     	<fieldset>
         	<legend>Simple form</legend>
@@ -31,9 +31,35 @@
 </div>
 <script>
 $(function(e) {
-	
-	var dropAvailable = true;
-	
+    
+	var option = { width: 150, items: [
+                { text: "Item One", icon: "sample-css/wi0126-16.gif", alias: "1-1", action: "" },
+                { text: "Item Two", icon: "sample-css/ac0036-16.gif", alias: "1-2", action: "" },
+                //this is normal menu item, menuAction will be called if this item is clicked on 
+                { text: "Item Three", icon: "sample-css/ei0021-16.gif", alias: "1-3", action: "" },
+                //this is a split line
+                { type: "splitLine" },
+                //this is a parent item, which has some sub-menu items
+                { text: "Group One", icon: "sample-css/wi0009-16.gif", alias: "1-4", type: "group", width: 170, items: [
+	                { text: "Group Three", icon: "sample-css/wi0054-16.gif", alias: "2-2", type: "group", width: 190, items: [
+		                { text: "Group3 Item One", icon: "sample-css/wi0062-16.gif", alias: "3-1", action: "" },
+		                { text: "Group3 Item Tow", icon: "sample-css/wi0063-16.gif", alias: "3-2", action: "" }
+	                ]
+	                },
+	                { text: "Group Two Item1", icon: "sample-css/wi0096-16.gif", alias: "2-1", action: "" },
+	                { text: "Group Two Item1", icon: "sample-css/wi0111-16.gif", alias: "2-3", action: "" },
+	                { text: "Group Two Item1", icon: "sample-css/wi0122-16.gif", alias: "2-4", action: "" }
+                ]
+                },
+                { type: "splitLine" },
+                { text: "Item Four", icon: "sample-css/wi0124-16.gif", alias: "1-5", action: "" },
+                { text: "Group Three", icon: "sample-css/wi0062-16.gif", alias: "1-6", type: "group", width: 180, items: [
+	                { text: "Item One", icon: "sample-css/wi0096-16.gif", alias: "4-1", action: "" },
+	                { text: "Item Two", icon: "sample-css/wi0122-16.gif", alias: "4-2", action: "" }
+                ]
+                }
+                ]
+        };
 	
     $( ".draggable" ).draggable({
 		cancel: false,
@@ -52,11 +78,12 @@ $(function(e) {
 				
 				// Add Click Event
 				$( $element ).mousedown(function(e) {
-					$( $element ).parent( ".element-container" ).css({
-						border: "1px solid #C03"
-					});
+
 				});
-	
+				
+				// Right Click Event
+				$element.contextmenu(option);
+				
 				// Set it to draggable
 				$element.draggable({
 					cancel: false,
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -7,10 +7,12 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/bootstrap.min.css');?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/jquery-ui.min.css');?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/font-awesome.min.css');?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/jquery.contextMenu.css');?>">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css');?>">
-    
+
 <script src="<?php echo base_url('assets/js/jquery-2.1.1.min.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/jquery.contextMenu.js'); ?>"></script>
 <script src="<?php echo base_url('assets/js/script.js'); ?>"></script>
 
 </head>
